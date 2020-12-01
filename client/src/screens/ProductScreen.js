edc0ae03ba8c41b48797eca909da2d4b2466c6bd
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-11-30 19:00:05 -0700

    playing around with match params. changed path to reference product name rather than id and used match.params.name in the ProductScreen find method. Will switch it back before next commit

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-	return <div>Product</div>
+const ProductScreen = ({ match }) => {
+	const product = products.find(
+		(product) => product.name === match.params.name
+	)
+	return <div>{product.name}</div>
 }
 
 export default ProductScreen
