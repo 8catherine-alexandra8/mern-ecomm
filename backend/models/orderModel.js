3b2c65259b412f768c568e2f221792cef1c02361
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-12-14 09:15:44 -0700

    add redux flow for create order. dispatch create order from place order screen to send order to database

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
 	{
-		customer        : {
+		user            : {
 			type     : mongoose.Schema.Types.ObjectId,
 			required : true,
 			ref      : 'User'
