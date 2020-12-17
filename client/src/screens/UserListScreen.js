eac16913783982f6c05612af9a3019cc0af9a0c3
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-12-17 10:55:24 -0700

    add backend routing for delete user. Add front end Redux flow for delete user functionality and implement functionality from UserListScreen

--- client/src/screens/UserListScreen.js
+++ client/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
 	const dispatch = useDispatch()
@@ -12,8 +12,12 @@ const UserListScreen = ({ history }) => {
 	const userList = useSelector((state) => state.userList)
     const { loading, error, users } = userList
     
-    const userLogin = useSelector((state) => state.userLogin)
-    const { userInfo } = userLogin    
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin    
+
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete    
+
 
 	useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
@@ -21,10 +25,12 @@ const UserListScreen = ({ history }) => {
         } else {
             history.push('/')
         }
-	}, [dispatch, userInfo, history])
+	}, [dispatch, userInfo, history, successDelete])
 
-    const deleteHandler = (id) => {
-        console.log('delete user')
+    const deleteHandler = (id, name) => {
+      if (window.confirm(`Are you sure you want to delete user ${name}?`)) {
+          dispatch(deleteUser(id))
+      }
     }
 	return (
             <>
@@ -68,7 +74,7 @@ const UserListScreen = ({ history }) => {
                   <Button
                     variant='danger'
                     className='btn-sm'
-                    onClick={() => deleteHandler(user._id)}
+                    onClick={() => deleteHandler(user._id, user.name)}
                   >
                     <i className='fas fa-trash'></i>
                   </Button>
